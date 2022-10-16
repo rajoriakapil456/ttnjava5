16b419c8d6a5c40b87167a7822feb82747766267
kapil rajoria <53654820+rajoriakapil456@users.noreply.github.com>
2022-10-16 13:22:00 +0530

    Update q11.java

--- q11.java
+++ q11.java
@@ -1,14 +1,10 @@
 import java.util.Map;
 public record Student (String name,Integer id ,Integer age)
 {
-    static int counter =0;
-    static void inc()
+    public static int oc = 0;
+    public Student()
     {
-
-    }
-    Student
-    {
-        inc();
+        oc++;
     }
 }
 class Question_11 {
@@ -17,6 +13,6 @@ class Question_11 {
         Student st1=new Student("kapil",1,20);
         Student st2=new Student("saurabh",1,20);
         System.out.println(st2.equals(st1));
-        System.out.println(Student.counter);
+        System.out.println(Student.oc);
     }
 }
